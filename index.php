073cee0d8e2b653f177b4210ca8066f8c3361248
Tejeshwar <work10tejeshwar@gmail.com>
2022-08-17 17:00:29 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PaiseDeBSDK!</title>
+    <link rel="stylesheet" href="CSS/indstyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+
+</head>
+<body>
+<header>
+<div class = "bg-image">
+    <div class = "bg-opacity">
+        <h1 >
+            PaiseDeBSDK!
+        </h1>
+    </div>    
+</div>
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="udhari.php">Udhari</a></li>
+        <li><a href="profile.php">Profile</a></li>
+        <li><a href="about.php">About</a></li>
+    </ul>
+</header>
+<div class = "bg-main">
+    <div class = "showcase">
+        <div><img class = "meme1" src="https://i.pinimg.com/originals/d0/91/78/d09178908bc1d94bdd4d13f19749ee4c.jpg" alt="Image Here"></div>
+        <div ><img class = "meme2" src="https://memegenerator.net/img/instances/85485445/dekh-bhai-udhari-band-hai.jpg" alt=""></div>
+        <div class = "para">
+            <p>Do you have a bhosdiwala in your group who never pays you back? <br> We are providing a foolproof 
+                solution to publically zalil that bhosdiwala to make him pay back. <br>
+            </p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat totam reprehenderit earum 
+                fuga ad necessitatibus minima vitae tempora aspernatur laboriosam.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta aperiam,
+                     ut laboriosam vero nobis ducimus cumque nemo quidem cum quasi.</p>
+        </div>
+    </div>
+    <div>
+        <div>
+            <div><img src="" alt="#"></div>
+            <div><p>Lorem ipsum dolor sit amet </p></div>
+        </div>
+        <div>
+            <div><img src="" alt=""></div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, sapiente!</div>
+        </div>
+        <div>
+            <div><img src="" alt=""></div>
+            <div>Lorem ipsum dolor sit amet consectetur.</div>
+        </div>
+    </div>
+</div>    
+    <footer class = "foot">
+        <div>
+            <div class = "symbol">
+                
+                <div class = "icons">
+                    <a href="http://instagram.com" class = "fa fa-instagram" target = "_blank"></a>
+                    <a href="http://facebook.com" class="fa fa-facebook" target = "_blank"></a>
+                    <a href="http://youtube.com" class="fa fa-youtube" target = "_blank"></a>
+                </div>
+            </div>
+            <div>
+                <a href="index.php">Home</a><br>
+                <a href="udhari.php">Udhari</a><br>
+                <a href="about.php">About</a><br>
+                <a href="feedback.php">Feedback</a><br>
+            </div>
+           
+                
+        </div>
+    </footer>
+
+
+</body>
+</html>
